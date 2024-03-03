23270cca5c5ce2ef12fdbc2982d8e39a8b7e14d4
zhaoyadong <zhaoyadong@baidu.com>
2024-03-03 15:44:25 +0800

    更新部署目

--- docs/assets/index-d4bed0c2.js
+++
@@ -1,1 +0,0 @@
-import{u as e,j as r}from"./index-d14ba294.js";const u=()=>{const t=e(o=>o.counter);return r.jsxs("div",{children:["About Page ",t]})};export{u as default};
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "react-zustand",
     build: {
         outDir: "docs",
     },
